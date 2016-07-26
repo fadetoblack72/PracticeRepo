4bbdc7025f5cf265ea33b427b6dd785c24321e29
Dylan Sturges <dylansturges@gmail.com>
2016-07-26 19:07:40 -0400

    another quick solution
    +1 practice

---
+++ getSum.py
@@ -0,0 +1,10 @@
+#silly way to add two numbers without actually using + or - operators
+#solution to https://leetcode.com/problems/sum-of-two-integers/
+
+def getSum(a, b):
+    """
+    :type a: int
+    :type b: int
+    :rtype: int
+    """
+    return sum([a, b])
